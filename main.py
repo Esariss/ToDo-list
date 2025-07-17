111c0e508623f39d72b559fd5b8272b3ae94a1ed
EsaRis <esaris1998@gmail.com>
2025-07-17 17:41:23 +0400

    исправление мелких багов

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import os, json
+import json
 from json import JSONDecodeError
 
 class ToDo:
@@ -6,6 +6,7 @@ class ToDo:
     task = None
     choice = None
     todo_list = []
+    program_activ = True
 
     def get_info(self):
         self.data = input("введите дату или время задачи: ")
@@ -20,11 +21,10 @@ class ToDo:
             json.dump(self.todo_list, file , ensure_ascii=False, indent=4)
 
     def info_to_python(self):
-        if os.path.exists("todo.json"):
-            with open("todo.json", "r", encoding="utf-8") as file:
-                self.todo_list = json.load(file)
-        else:
-            self.todo_list = []
+
+        with open("todo.json", "r", encoding="utf-8") as file:
+            self.todo_list = json.load(file)
+
 
         for index, el in enumerate(self.todo_list, start=1):
             print(f"{index}. Дата: {el['data']} | Задача: {el['task']}")
@@ -39,27 +39,29 @@ class ToDo:
 завершить программу (-3- , stop , конец)
 -----------------------------
         """)
+        while self.program_activ:
+            try:
+                while True:
+                    self.choice = input("выберите действие:")
 
-        try:
-            while True:
-                self.choice = input("выберите действие:")
+                    if self.choice == "1":
+                        self.get_info()
+                        self.info_to_json()
 
-                if self.choice == "1":
-                    self.get_info()
-                    self.info_to_json()
+                    elif self.choice == "2":
+                        self.info_to_python()
 
-                elif self.choice == "2":
-                    self.info_to_python()
+                    elif self.choice in ["3", "stop", "конец"]:
+                        self.program_activ = False
+                        break
+                    else:
+                        print("извините, запрос не был понят.\nпожалуйста повторите попытку")
+            except JSONDecodeError:
+                print("в списке пока нет задач")
 
-                elif self.choice == "3" or "stop" or "конец":
-                    break
-                else:
-                    print("извините, запрос не был понят.\n пожалуйста повторите попытку")
-        except JSONDecodeError:
-            print("в списке пока нет задач")
-
-        except KeyboardInterrupt:
-            print("\nпрограмма завершена ")
+            except KeyboardInterrupt:
+                self.program_activ = False
+                print("\nпрограмма завершена ")
 
 test = ToDo()
 test.start_program()
